ac5126a0f3a8919cf18feb009d81ddce4bbb50a7
gitgudkidd <Benjamin.Poveda@hotmail.com>
2021-04-29 17:53:54 -0400

    Generamos tabla con javascript

--- js/index.js
+++ js/index.js
@@ -15,9 +15,67 @@ tinymce.init({
     'removeformat | help',
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
+  const pokemones = [];
   
-  
-  
+  const cargarTabla = ()=>{
+    
+    //1-Obtener una referencia a la tabla
+    let tbody = document.querySelector("#tabla-tbody");
+    
+    //Eliminar todos los elementos que tenga el tbody
+    tbody.innerHTML =""
+
+    //2-Recorrer la lista
+    for(let i=0; i < pokemones.length; ++i){
+      let p = pokemones[i];
+      
+      //3-Por cada pokemon generar una fila (tr)
+      let tr = document.createElement("tr")
+      
+      //4-Por cada atributo, generar celdas (td)
+      let tdNRO = document.createElement("td");
+      tdNRO.innerText = (i+1)
+      let tdNombre = document.createElement("td");
+      tdNombre.innerText = p.nombre;
+      let tdTipo = document.createElement("td");
+      
+      let icono = document.createElement("i");
+      if(p.tipo == "fuego"){
+        //<i class="fas fa-fire"></i>
+        //Agregar clases a un elemento
+        icono.classList.add("fas","fa-fire","text-danger","fa-3x")
+      }else if(p.tipo == "planta"){
+        //<i class="fas fa-leaf"></i>
+        icono.classList.add("fas","fa-leaf","text-success","fa-3x")
+      }else if(p.tipo == "electrico"){
+        //<i class="fas fa-bolt"></i>
+        icono.classList.add("fas","fa-bolt","text-warning","fa-3x")
+      }else if(p.tipo == "agua"){
+        //<i class="fas fa-tint"></i>
+        icono.classList.add("fas","fa-tint","text-primary","fa-3x")
+      }else if(p.tipo == "normal"){
+        //<i class="fas fa-star"></i>
+        icono.classList.add("fas","fa-star","text-info","fa-3x")
+      }
+      tdTipo.classList.add("text-center");
+      tdTipo.appendChild(icono);
+      
+      let tdDescripcion = document.createElement("td");
+      tdDescripcion.innerHTML = p.descripcion;
+      let tdAcciones = document.createElement("td");
+      
+      //5-Agregar las celdas al tr
+      tr.appendChild(tdNRO);
+      tr.appendChild(tdNombre);
+      tr.appendChild(tdTipo);
+      tr.appendChild(tdDescripcion);
+      tr.appendChild(tdAcciones);
+      
+      //6-Agregar el tr a la tabla
+      tbody.appendChild(tr);
+    }
+
+  };
 
 document.querySelector("#registrar-btn").addEventListener("click", () =>{
     //value es para obtener el valor de los input de texto
@@ -29,5 +87,17 @@ document.querySelector("#registrar-btn").addEventListener("click", () =>{
     //el tipo se obtiene igual que los input
     let tipo = document.querySelector("#tipo-select").value;
 
-    console.log("Hola Mundo", nombre, descripcion, legendario, tipo)
+    //Como crear un objeto
+    let pokemon = {};
+    pokemon.nombre = nombre;
+    pokemon.descripcion = descripcion;
+    pokemon.legendario = legendario;
+    pokemon.tipo = tipo;
+
+    //Como guardar en una lista de elementos
+    pokemones.push(pokemon); // append
+    cargarTabla();
+    Swal.fire("Exito!","Pokemon registrado","success");
+
+    
 } );
